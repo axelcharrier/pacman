f4d578d3804ae74ce464caa0d04cd9ef25d737f5
achar <axel.charrier1@ecoles-epsi.net>
2025-04-20 15:11:35 +0200

    commit final

--- ghost.h
+++ ghost.h
@@ -3,63 +3,107 @@
 
 class Ghost
 {
-public:
-  Ghost(int x, int y) : posX(x), posY(y) {}
-
-  void draw(sf::RenderWindow &window, const std::string &color)
-  {
-    sf::CircleShape ghostShape(12.5f);
-    ghostShape.setPosition(posX * CELLULE_SIZE, posY * CELLULE_SIZE);
-
-    if (color == "red")
-      ghostShape.setFillColor(sf::Color::Red);
-    else if (color == "blue")
-      ghostShape.setFillColor(sf::Color::Blue);
-    else if (color == "green")
-      ghostShape.setFillColor(sf::Color::Green);
-    else if (color == "white")
-      ghostShape.setFillColor(sf::Color::White);
-
-    window.draw(ghostShape);
-  }
-
-  void move(std::string map[MAP_HEIGHT])
-  {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dir(0, 3); // 0: up, 1: down, 2: left, 3: right
-
-    int newX = posX;
-    int newY = posY;
-
-    switch (dir(gen))
-    {
-    case 0:
-      newY--;
-      break; // Up
-    case 1:
-      newY++;
-      break; // Down
-    case 2:
-      newX--;
-      break; // Left
-    case 3:
-      newX++;
-      break; // Right
-    }
-
-    // Check if the new position is valid
-    if (newX >= 0 && newX < MAP_WIDTH && newY >= 0 && newY < MAP_HEIGHT && map[newY][newX] != '#')
-    {
-      posX = newX;
-      posY = newY;
-    }
-  }
-
-  int getX() const { return posX; }
-  int getY() const { return posY; }
-
 private:
   int posX;
   int posY;
+  int previousDirection = -1; // -1 signifie aucune direction précédente
+
+public:
+  Ghost(int x, int y) : posX(x), posY(y) {}
+
+  int getX() const { return posX; } // Getter pour la position X
+  int getY() const { return posY; } // Getter pour la position Y
+
+  void draw(sf::RenderWindow &window, const std::string &color)
+  {
+    sf::CircleShape ghostShape(12.5f); // Create a circle shape for the ghost
+    if (color == "red")
+      ghostShape.setFillColor(sf::Color::Red); // Set the color of the ghost to red
+    else if (color == "blue")
+      ghostShape.setFillColor(sf::Color::Blue); // Set the color of the ghost to blue
+    else if (color == "green")
+      ghostShape.setFillColor(sf::Color::Green); // Set the color of the ghost to green
+
+    ghostShape.setPosition(posX * CELLULE_SIZE, posY * CELLULE_SIZE);
+    window.draw(ghostShape); // Draw the ghost
+  }
+
+  void teleport(int x, int y, std::string map[MAP_HEIGHT])
+  {
+    // Effacez l'ancienne position du fantôme dans la carte
+    map[posY][posX] = ' ';
+
+    // Mettez à jour les coordonnées du fantôme
+    posX = x;
+    posY = y;
+
+    // Placez le fantôme à la nouvelle position dans la carte
+    map[posY][posX] = '1'; // Assurez-vous que '1' correspond au fantôme
+  }
+
+  void move(int direction, std::string map[MAP_HEIGHT])
+  {
+    previousDirection = direction; // Store the previous direction
+    switch (direction)
+    {
+    case 0: // Up
+      posY--;
+      break;
+    case 1: // Down
+      posY++;
+      break;
+    case 2: // Left
+      posX--;
+      break;
+    case 3: // Right
+      posX++;
+      break;
+    default:
+      break;
+    }
+
+    // Ensure the ghost stays within the bounds of the grid
+    if (posX < 0)
+      posX = 0;
+    if (posX >= MAP_WIDTH)
+    {
+      posX = MAP_WIDTH - 1;
+    }
+    if (posY < 0)
+    {
+      posY = 0;
+    }
+    if (posY >= MAP_HEIGHT)
+    {
+      posY = MAP_HEIGHT - 1;
+    }
+
+    // Ensure the ghost doesn't move through walls
+    if (map[posY][posX] == '#' || map[posY][posX] == 'O' || map[posY][posX] == 'L' || map[posY][posX] == 'R')
+    {
+      // Move back to the previous position
+      switch (direction)
+      {
+      case 0: // Up
+        posY++;
+        break;
+      case 1: // Down
+        posY--;
+        break;
+      case 2: // Left
+        posX++;
+        break;
+      case 3: // Right
+        posX--;
+        break;
+      default:
+        break;
+      }
+    }
+  }
+
+  int getPreviousDirection() const
+  {
+    return previousDirection;
+  }
 };
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int const CELLULE_SIZE = 25;
 #include <iostream>
 #include "pacman.h"
 #include "ghost.h"
+#include <random>
 
 bool win(std::string map[MAP_HEIGHT])
 {
@@ -24,6 +25,15 @@ bool win(std::string map[MAP_HEIGHT])
   return true;
 }
 
+bool loose(std::string map[MAP_HEIGHT], int xGhost, int yGhost, int xPacman, int yPacman)
+{
+  if (xGhost == xPacman && yGhost == yPacman)
+  {
+    return true;
+  }
+  return false;
+}
+
 bool bonusPoint(std::string map[MAP_HEIGHT], int x, int y)
 {
   if (map[y][x] == 'P')
@@ -37,6 +47,8 @@ int main()
 {
   int SCORE = 0;
   char DIRECTION = ' ';
+  int TIME = 0;
+  bool perdu = false;
 
   sf::Font font;
   font.loadFromFile("pacman-art/arial.ttf");
@@ -49,7 +61,6 @@ int main()
   scoreText.setFillColor(sf::Color::White);
   scoreText.setPosition(600, 10);
   Ghost ghost1(10, 10); // Exemple de position initiale
-  Ghost ghost2(12, 12); // Un autre fantôme si nécessaire
   // Create the map
   std::string map[MAP_HEIGHT] = {
       "#######################",
@@ -83,6 +94,7 @@ int main()
   // run the program as long as the window is open
   while (window.isOpen())
   {
+    TIME += 1;
     // check all the window's events that were triggered since the last iteration of the loop
     sf::Event event;
     while (window.pollEvent(event))
@@ -97,8 +109,9 @@ int main()
     // on créer chaque element de la map
     sf::RectangleShape rectangle(sf::Vector2f(CELLULE_SIZE, CELLULE_SIZE)); // les murs
     sf::CircleShape point(2.5f);                                            // les boules
-    if (win(map) == false)
+    if (win(map) == false && perdu == false)
     {
+      // Draw the map
       for (int i = 0; i < MAP_HEIGHT; i++)
       {
         for (int j = 0; j < MAP_WIDTH; j++)
@@ -108,6 +121,66 @@ int main()
             rectangle.setPosition(j * CELLULE_SIZE, i * CELLULE_SIZE);
             rectangle.setFillColor(sf::Color::Blue);
             window.draw(rectangle);
+          }
+          if (map[i][j] == '1')
+          {
+            ghost1.teleport(11, 8, map); // Teleport the ghost to the position of '1'
+          }
+          if (map[i][j] == 'O')
+          {
+            rectangle.setPosition(j * CELLULE_SIZE, i * CELLULE_SIZE);
+            rectangle.setFillColor(sf::Color::White);
+            window.draw(rectangle);
+          }
+          if (map[i][j] == 'P')
+          {
+            Pacman pacman(j, i); // Create Pacman at the position of 'P'
+            pacman.draw(window); // Draw Pacman
+          }
+          if (map[i][j] == '.')
+          {
+            point.setPosition(j * CELLULE_SIZE + 11.25, i * CELLULE_SIZE + 11.25);
+            point.setFillColor(sf::Color::Yellow);
+            window.draw(point);
+          }
+          if (map[i][j] == 'C')
+          {
+            rectangle.setPosition(j * CELLULE_SIZE, i * CELLULE_SIZE);
+            rectangle.setFillColor(sf::Color::Red);
+            window.draw(rectangle);
+          }
+        }
+      }
+      for (int i = 0; i < MAP_HEIGHT; i++)
+      {
+        for (int j = 0; j < MAP_WIDTH; j++)
+        {
+          if (map[i][j] == '#')
+          {
+            rectangle.setPosition(j * CELLULE_SIZE, i * CELLULE_SIZE);
+            rectangle.setFillColor(sf::Color::Blue);
+            window.draw(rectangle);
+          }
+          if (map[i][j] == '1')
+          {
+            if (TIME == 15)
+            {
+              ghost1.teleport(11, 8, map); // Teleport the ghost to the position of '1'
+            }
+            if (TIME > 15)
+            {
+              int randomDirection;
+              do
+              {
+                randomDirection = rand() % 4;                                          // Génère une direction aléatoire (0 à 3)
+              } while ((randomDirection == 0 && ghost1.getPreviousDirection() == 1) || // Haut et Bas opposés
+                       (randomDirection == 1 && ghost1.getPreviousDirection() == 0) || // Bas et Haut opposés
+                       (randomDirection == 2 && ghost1.getPreviousDirection() == 3) || // Gauche et Droite opposés
+                       (randomDirection == 3 && ghost1.getPreviousDirection() == 2)); // Droite et Gauche opposés
+
+              ghost1.move(randomDirection, map); // Déplacez le fantôme
+            }
+            ghost1.draw(window, "red"); // Draw the ghost
           }
           if (map[i][j] == 'O')
           {
@@ -152,6 +225,24 @@ int main()
           }
         }
       }
+      // compteur de pacman sur la map
+      int compteur = 0;
+      for (int i = 0; i < MAP_HEIGHT; i++)
+      {
+        for (int j = 0; j < MAP_WIDTH; j++)
+        {
+          if (map[i][j] == 'P')
+          {
+            compteur++;
+          }
+        }
+      }
+      if (compteur > 1)
+      {
+        perdu = true; // Pacman is dead
+      }
+
+      perdu = loose(map, ghost1.getX(), ghost1.getY(), pacmanX, pacmanY); // Check if Pacman is dead
       // I want to make a little delay to see the pacman moving
       std::this_thread::sleep_for(std::chrono::milliseconds(75)); // 50 milliseconds delay
       // Move the pacman to the right
@@ -266,8 +357,10 @@ int main()
         }
       }
     }
-    else
+    if (win(map) == true)
     {
+      std::this_thread::sleep_for(std::chrono::milliseconds(75)); // 50 milliseconds delay
+
       window.draw(text);
 
       // Display the score
@@ -275,10 +368,14 @@ int main()
       scoreText.setPosition(350, 350); // Position the score text
       window.draw(scoreText);
     }
+    if (perdu == true)
+    {
+      // Display the lose message
+      text.setString("You lose!");
+      text.setPosition(300, 250); // Position the text
+      window.draw(text);
+    }
 
-    // Display the win message
-
-    // end the current frame
     window.display();
   }
   return 0;
